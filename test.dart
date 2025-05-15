54dd95f6dc7724b8574d1fac0c8267ce091f715c
mariaquintero6 <aleja.qc12@gmail.com>
2025-05-15 08:23:05 -0500

    examen

--- test.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  String? nombre = 'Libro';
-  double? precio = 10.5;
-  int? cantidad = 2;
-
-  if (nombre != null && precio != null && cantidad != null) {
-    print('Todos los valores son válidos');
-  }
-}
